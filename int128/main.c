991fdab170e1e85923cf1283a5e18acb429b3055
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2021-10-29 14:31:42 +0200

    Update tests for div/rem on 128-bit
    In particular corrects a bug: I was doing an int128_rshift that is a
    signed shift, while an unsigned one is needed.
    It has been added in int128.h.

---
+++ int128/main.c
@@ -0,0 +1,91 @@
+/* Make sure the Int128 div and rem functions behave as expected */
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "int128.h"
+/* quick and dirty c include */
+#include "int128.c"
+
+uint64_t r(void)
+{
+    uint64_t x = random() << 32 | random();
+    x *= random() % 2 ? -1 : 1;
+    return x;
+}
+
+int main(void)
+{
+    srandom(122);
+
+    for (int i = 0; i < 256; i++) {
+        Int128 x = int128_make128(r() & 0xffff, 0);
+        Int128 y = int128_make128(i+1, 0);
+        Int128 du = int128_divu(x, y);
+        printf("du 0x%016lx%016lx\n", int128_gethi(du), int128_getlo(du));
+        Int128 ds = int128_divs(x, y);
+        printf("ds 0x%016lx%016lx\n", int128_gethi(ds), int128_getlo(ds));
+        Int128 ru = int128_remu(x, y);
+        printf("ru 0x%016lx%016lx\n", int128_gethi(ru), int128_getlo(ru));
+        Int128 rs = int128_rems(x, y);
+        printf("rs 0x%016lx%016lx\n", int128_gethi(rs), int128_getlo(rs));
+    }
+    for (int i = 0; i < 100000; i++) {
+        Int128 x = int128_make128(r() & 0xfffffff, 0);
+        Int128 y = int128_make128(i+1, 0);
+        Int128 du = int128_divu(x, y);
+        printf("du 0x%016lx%016lx\n", int128_gethi(du), int128_getlo(du));
+        Int128 ds = int128_divs(x, y);
+        printf("ds 0x%016lx%016lx\n", int128_gethi(ds), int128_getlo(ds));
+        Int128 ru = int128_remu(x, y);
+        printf("ru 0x%016lx%016lx\n", int128_gethi(ru), int128_getlo(ru));
+        Int128 rs = int128_rems(x, y);
+        printf("rs 0x%016lx%016lx\n", int128_gethi(rs), int128_getlo(rs));
+    }
+    for (int i = 0; i < 100000; i++) {
+        Int128 x = int128_make128(r(), 0);
+        Int128 y = int128_make128(i+1, 0);
+        Int128 du = int128_divu(x, y);
+        printf("du 0x%016lx%016lx\n", int128_gethi(du), int128_getlo(du));
+        Int128 ds = int128_divs(x, y);
+        printf("ds 0x%016lx%016lx\n", int128_gethi(ds), int128_getlo(ds));
+        Int128 ru = int128_remu(x, y);
+        printf("ru 0x%016lx%016lx\n", int128_gethi(ru), int128_getlo(ru));
+        Int128 rs = int128_rems(x, y);
+        printf("rs 0x%016lx%016lx\n", int128_gethi(rs), int128_getlo(rs));
+    }
+    for (int i = 0; i < 100000; i++) {
+        Int128 x = int128_make128(r(), r());
+        Int128 y = int128_make128(r(), i);
+        Int128 du = int128_divu(x, y);
+        printf("du 0x%016lx%016lx\n", int128_gethi(du), int128_getlo(du));
+        Int128 ds = int128_divs(x, y);
+        printf("ds 0x%016lx%016lx\n", int128_gethi(ds), int128_getlo(ds));
+        Int128 ru = int128_remu(x, y);
+        printf("ru 0x%016lx%016lx\n", int128_gethi(ru), int128_getlo(ru));
+        Int128 rs = int128_rems(x, y);
+        printf("rs 0x%016lx%016lx\n", int128_gethi(rs), int128_getlo(rs));
+    }
+    for (int i = 0; i < 100000; i++) {
+        Int128 x = int128_make128(r(), r());
+        Int128 y = int128_make128(r(), r());
+        Int128 du = int128_divu(x, y);
+        printf("du 0x%016lx%016lx\n", int128_gethi(du), int128_getlo(du));
+        Int128 ds = int128_divs(x, y);
+        printf("ds 0x%016lx%016lx\n", int128_gethi(ds), int128_getlo(ds));
+        Int128 ru = int128_remu(x, y);
+        printf("ru 0x%016lx%016lx\n", int128_gethi(ru), int128_getlo(ru));
+        Int128 rs = int128_rems(x, y);
+        printf("rs 0x%016lx%016lx\n", int128_gethi(rs), int128_getlo(rs));
+    }
+
+    for (int i = 0; i < 128; i++) {
+        Int128 y = int128_make128(r(), r());
+        /* shift fails if shamt > 127, no assert in function */
+        Int128 r = int128_rlshift(y, i);
+        printf("r%3d 0x%016lx%016lx\n", i, int128_gethi(r), int128_getlo(r));
+    }
+    return 0;
+}
