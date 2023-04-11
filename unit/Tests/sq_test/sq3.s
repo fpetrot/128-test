1bdedc91cf20287ad75ad5ea1268162a6dd8bd82
FEZEU YOUMBI Emmanuel <yfezeu.emma@gmail.com>
2023-04-11 17:55:25 +0200

    TOPIC : Debut Tests génération assembleur des instructions 128I
    J'ai crée des packages pour chaque instruction, dans ceux-ci se trouvent
    un ou plusieurs fichiers de test contenant le code  de l'instruction.J'ai
    exécuté les commandes permettant de générer l'assembleur, puis j'ai fait un
    objdump pour regénérer le programme de base sur le file assembleur généré.
    Les sorties semblaient être identiques au fichier assembleur de base

---
+++ unit/Tests/sq_test/sq3.s
@@ -0,0 +1,17 @@
+x :
+    sq t0,  0(t1)
+    sq t0, 16(t1)
+    sq t0, 20(t2)
+    sq t0, 35(t2)
+
+y :
+    sq t1,  0(t2)
+    sq t0, 16(t1)
+    sq t3, 20(t2)
+    sq t0, 35(t2)
+
+z :
+    sq t1,  5(t2)
+    sq t0, 16(t1)
+    sq t3, 25(t2)
+    sq t0, 35(t2)
---
+++ unit/Tests/srli_test/srli0.s
@@ -0,0 +1,8 @@
+x:
+    srli t0, t0, 2
+
+y:
+    srli t0, t1, 4
+
+z:
+    srli t2, t5, 11
