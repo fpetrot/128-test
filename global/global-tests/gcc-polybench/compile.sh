c08723de93034ded9689278c84b03f12e5dbc397
lgaver2 <crumb_thorny_0q@icloud.com>
2025-06-17 14:36:48 +0000

    refractor: change localisation to global/gcc-polybench

--- global/global-tests/gcc-polybench/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Compile each .c file in subdirectories, placing the executable in the same directory as the .c file
-
-find . -type f -name "*.c" | while read -r file; do
-    dir=$(dirname "$file")
-    name=$(basename "$file" .c)
-    gcc "$file" -o "$dir/$name" -lm
-    ./"$dir/$name" > "$dir/$name".exp 
-done
