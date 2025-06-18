324519370f9ecbb9e4629d9b82605b8b6822cc09
lgaver2 <crumb_thorny_0q@icloud.com>
2025-06-18 13:46:34 +0000

    update: change utils/compile.sh to be runnable in utils/ directory

--- global/utils/compile.sh
+++ global/utils/compile.sh
@@ -3,11 +3,11 @@
 # and save res as a .exp file 
 
 mkdir bin
-find ./gcc-polybench -type f -name "*.c" | while read -r file; do
+find ../gcc-polybench -type f -name "*.c" | while read -r file; do
     dir=$(dirname "$file")
-    dir=${dir#./gcc-polybench/}
+    dir=${dir#../gcc-polybench/}
     name=$(basename "$file" .c)
-    gcc -I./utils/headers "$file" -o bin/"$name" -lm || exit 1
-    ./bin/"$name" > ./global-tests/examples/polybench/"$dir/$name".exp
+    gcc -I./headers "$file" -o bin/"$name" -lm || exit 1
+    ./bin/"$name" > ../global-tests/examples/polybench/"$dir/$name".exp
 done
 rm -rf bin
