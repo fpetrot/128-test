cde41236075c2c45d74e2fd8caf9dc57b91e0239
Julien THILLARD <julien.thillard@univ-grenoble-alpes.fr>
2026-06-17 17:22:30 +0200

    refactor: Use a wrapper in argv0 test instead of hardcoded value in riscvbarelib

--- global/argv0/argv0.c
+++ global/argv0/argv0.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int real_main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
+}
+
+int main()
+{
+	char *argv = "128bits";
+	return real_main(1, &argv);
 }
