c8d48cb93f7f58e86381830f748fd66131c622a0
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2026-07-24 16:50:32 +0200

    unit: update gcc and qemu options
    Since we want to support some new extensions (cmo, address generation and
    such), we need to update both gcc and qemu invocations.

---
+++ unit/generate_zbs_imm.py
@@ -0,0 +1,55 @@
+#!/bin/env python3
+
+# Full range testing of the register to register 128-bit shifts
+# First argument is the number of random numbers to draw, above
+# 2 you have a good chance to have positive and negative numbers
+# for testing the right arithmetic shift
+
+import sys
+from random import randint
+
+_typedir = {
+    128: '.octa',
+    64: '.dword',
+    32: '.word'
+}
+
+if __name__ == "__main__":
+    if len(sys. argv) != 2:
+        print(f'Usage: {sys.argv[0]} n\n')
+        sys.exit(1)
+    datacnt = int(sys.argv[1])
+    datasize = 128
+
+    data = open("unit_tests_b/test_zbs_imm.S", "w")
+    data.write('''
+#include "exit.S"
+''')
+    data.write(".section .data\n")
+    data.write(f"tab_size: {_typedir[datasize]} {datacnt}\n")
+    data.write("tab_start:\n")
+    values = []
+    for _ in range(datacnt):
+        values.append(randint(0, 2**datasize - 1))
+        data.write(f"{_typedir[datasize]} 0x{values[_]:032x}\n")
+        data.write(f"#0b{values[_]:0128b}\n")
+
+    data.write(".section .text\n")
+    data.write('''
+.section .text.init,"ax",@progbits
+.globl _start
+_start:
+''')
+
+    for _ in  range(datacnt):
+        data.write(f"la t0, tab_start\n")
+        for __ in range(datasize):
+            shamt = __
+            v = values[_] | ((1 << shamt) & 0xffffffffffffffffffffffffffffffff)
+            offset = int(_ * datasize/8)
+            data.write(f"lq t1, {offset}(t0)\n")
+            data.write(f"bseti t2, t1, {__} \n")
+            data.write(f"//prgchk reg t2 == 0x{v&0xffffffffffffffffffffffffffffffff:032x}\n")
+
+    data.write('j exit')
+    data.close()
--- unit/prgchk.py
+++ unit/prgchk.py
@@ -62,8 +62,8 @@ def parse_regchk(a: CheckParseArgs) -> str:
 break {a.srcfile}:{a.lineno} if ${reg} {op_opposite[op]} {val}
 commands
 silent
-    output "Register check failed line {a.lineno}, expected {reg} {op} {val} (= {hex(val)}), got :"
-    output/x ${reg}
+    output "Register check failed line {a.lineno}, expected {reg} {op} {val} (= {hex(val)}), got: "
+    output /x ${reg}
     kill
     quit 1
 end
@@ -79,7 +79,7 @@ def parse_varchk(a: CheckParseArgs) -> str:
 break {a.srcfile}:{a.lineno} if ((unsigned long){varname}) {op_opposite[op]} {val}
 commands
 silent
-    output "Variable value check condition broken line {a.lineno}, expected {varname} {op} {val}, got :"
+    output "Variable value check condition broken line {a.lineno}, expected {varname} {op} {val}, got: "
     print /x (unsigned long){varname}
     kill
     quit 1
