cfc96878d8ebab022a14291381f67fa4e8db1951
Julien THILLARD <julien.thillard@univ-grenoble-alpes.fr>
2026-07-24 12:01:47 +0200

    fix: global tests now works completly on rv128, rv64 and rv32

--- global/basic_arith_64/basic_arith_64.c
+++ global/basic_arith_64/basic_arith_64.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#if __riscv_xlen == 128
+#define PF64 "ld"
+#else
+#define PF64 "lld"
+#endif
+
 int main()
 {
 	uint64_t a = 1200000000;
 	uint64_t b = 340000005;
-	printf("1200000000 + 340000005 = %ld\n", a + b);
-	printf("1200000000 - 340000005 = %ld\n", a - b);
+	printf("1200000000 + 340000005 = %"PF64"\n", a + b);
+	printf("1200000000 - 340000005 = %"PF64"\n", a - b);
 	a = 156000;
 	b = 25678;
-	printf("156000 * 25678 = %ld\n", a * b);
-	printf("156000 / 25678 = %ld\n", a / b);
-	printf("15600 modulo 25678 = %ld\n", a % b);
+	printf("156000 * 25678 = %"PF64"\n", a * b);
+	printf("156000 / 25678 = %"PF64"\n", a / b);
+	printf("15600 modulo 25678 = %"PF64"\n", a % b);
 }
